373bb08240ac379b395b127fdd94dc6aee304228
Birgit Croux <birgitta.croux.u1690@student.hogent.be>
2015-05-20 10:32:09 +0200

    Aanpassing deployment demo-app

--- deelopdracht02/wisa/demoapp/db_setup.ps1
+++ deelopdracht02/wisa/demoapp/db_setup.ps1
@@ -1,5 +1,5 @@
 Import-Module SqlPs
-Set-Location SQLSERVER:\SQL\vm-001\SQLEXPRESS
-Invoke-Sqlcmd -InputFile "C:\Users\klant_testproject\createdb_contoso.sql"
-Set-Location SQLSERVER:\SQL\vm-001\SQLEXPRESS\Databases\ContosoUniversity
-Invoke-Sqlcmd -InputFile "C:\Users\klant_testproject\Grant.sql"
+Set-Location SQLSERVER:\SQL\vagrant-2012-r2\SQLEXPRESS
+Invoke-Sqlcmd -InputFile "C:\vagrant\createdb_contoso.sql"
+Set-Location SQLSERVER:\SQL\vagrant-2012-r2\SQLEXPRESS\Databases\ContosoUniversity
+Invoke-Sqlcmd -InputFile "C:\vagrant\Grant.sql"
